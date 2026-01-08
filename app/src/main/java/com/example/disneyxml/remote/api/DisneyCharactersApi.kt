d6b965ff43e6d68d1bf6d7487b8a51cf033a78bb
Andrei <andreicampigotto@icloud.com>
2026-01-07 23:12:31 -0500

    resolvin dependency problems

--- app/src/main/java/com/example/disneyxml/remote/api/DisneyCharactersApi.kt
+++ app/src/main/java/com/example/disneyxml/remote/api/DisneyCharactersApi.kt
@@ -4,11 +4,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 interface DisneyCharactersApi {
-    @Provides
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl("https://api.disneyapi.dev/character")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+//    @Provides
+//    fun provideRetrofit(): Retrofit {
+//        return Retrofit.Builder()
+//            .baseUrl("https://api.disneyapi.dev/character")
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//    }
 }
